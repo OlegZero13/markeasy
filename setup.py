36667e7017c83d46cab6126ef0276c8e9772e617
OlegZero13 <oleg.zero13@gmail.com>
2020-02-22 23:03:28 +0100

    Fixed some spelling mistakes.

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setuptools.setup(
     author_email="oleg@zerowithdot.com",
     description="Easy implementation of Hidden Markov Model",
     long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/OlegZero13/markeasy/archive/0.1.0.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
@@ -20,5 +19,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requries='>=3.6',
+    python_requires='>=3.6',
 )
